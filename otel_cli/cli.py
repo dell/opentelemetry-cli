aa35687532483a8576385c18efac00e7ec7630a2
Moshi Binyamini <moshi.binyamini@emc.com>
2022-05-31 11:51:22 +0300

    Fix pytest options

--- otel_cli/cli.py
+++ otel_cli/cli.py
@@ -7,7 +7,7 @@ import click
 def main(args=None):
     """Console script for otel_python_cli."""
     click.echo("Replace this message by putting your code into "
-               "otel_python_cli.cli.main")
+               "otel-cli.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
