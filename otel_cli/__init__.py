ade89c926850ab2abe5198ae9889426398d05e6c
Moshi Binyamini <moshi.binyamini@emc.com>
2022-06-24 01:49:41 +0300

    🔖 Bump version: 0.0.1 → 0.1.0

--- otel_cli/__init__.py
+++ otel_cli/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Moshi Binyamini"""
 __email__ = "moshi.binyamini@dell.com"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://eos2git.cec.lab.emc.com/binyam/otel-client-python",
-    version="0.0.1",
+    version="0.1.0",
     zip_safe=False,
 )
