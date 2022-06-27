32590c085d984ab9b6496dbb66d32420cb4112d5
Moshi Binyamini <moshi.binyamini@emc.com>
2022-06-27 13:40:14 +0300

    ✅ Add tests for utils module

---
+++ tests/test_utils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""Tests for `otel-cli.utils` module."""
+
+import pytest
+from otel_cli import utils
+
+
+def test_remove_prefix(monkeypatch):
+    """Test utils.remove_prefix"""
+    assert utils.remove_prefix("int:test", "int:") == "test"
+    assert utils.remove_prefix("float:test", "float:") == "test"
+    assert utils.remove_prefix("noprefix", "bool:") == "noprefix"
+
+
+def test_parse_attributes():
+    attributes = utils.parse_attributes([
+        "key1=value",
+        "int:key2=100",
+        "float:key3=0.1",
+        "bool:key4=yes",
+        "bool:key5=0",
+    ])
+    assert attributes["key1"] == "value"
+    assert type(attributes["key2"]) is int
+    assert attributes["key2"] == 100
+    assert type(attributes["key3"]) is float
+    assert attributes["key3"] == 0.1
+    assert type(attributes["key4"]) is bool
+    assert type(attributes["key5"]) is bool
+    assert attributes["key4"] is True
+    assert attributes["key5"] is False
+
+    with pytest.raises(ValueError):
+        utils.parse_attributes(["int:key=NotANumber"])
+
+    with pytest.raises(ValueError):
+        utils.parse_attributes(["bool:key=NotABool"])
