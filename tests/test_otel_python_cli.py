322fef54fdaf1e9e25c38574677e5ca260e41e34
Moshi Binyamini <moshi.binyamini@emc.com>
2022-08-10 15:59:09 +0300

    ✅ Fix basic CLI test

--- tests/test_otel_python_cli.py
+++ tests/test_otel_python_cli.py
@@ -34,7 +34,7 @@ def test_command_line_interface():
     assert "main" in result.output
     help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+    assert "Show this message and exit." in help_result.output
 
 
 def test_generate_trace_id():
